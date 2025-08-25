7e3898a0e8f78d36b2397bd6aacb5d1b85e743b8
Yamaguchi Natsuko <gachaben2025@gmail.com>
2025-08-25 14:38:58 +0900

    chore(cleanup): remove egg from kontyuItems data

--- src/data/kontyuItems.js
+++ src/data/kontyuItems.js
@@ -1,22 +1,10 @@
 const kontyuItems = [];
 
 // 名前のテンプレ（仮）
-const baseNames = {
-  "001": "きらきらたまご",
-  "002": "きらきらたまご",
-  "003": "むしたまご3",
-  "004": "むしたまご4",
-  "005": "むしたまご5",
-  "006": "むしたまご6",
-  "007": "むしたまご7",
-  "008": "むしたまご8",
-  "009": "むしたまご9",
-  "010": "むしたまご10"
-};
 
-const stages = ["egg", "youchuu", "sanagi", "seichuu", "premium"];
+const stages = ["youchuu", "sanagi", "seichuu", "premium"];
 const stageNames = {
-  egg: "たまご",
+
   youchuu: "ようちゅう",
   sanagi: "さなぎ",
   seichuu: "せいちゅう",
